8af9fbb16abb90030dd68d300a6f2f544ffdbbe6
Derek Bankieris <derek.r.bankieris@nasa.gov>
2014-03-17 14:11:55 +0000

    refs #232 Added support for Macs in example sims. Updated svn:ignore lists.

--- sims/SIM_device_only/S_overrides.mk
+++ sims/SIM_device_only/S_overrides.mk
@@ -1,13 +1,25 @@
-# Change these to fit your system
+# Point this to your copy of IDF
 IDF_HOME = ${HOME}/idf
-LIBUSB_HOME = /usr/include/libusb-1.0
 
-# Don't change anything below this line
-common = -I${IDF_HOME}/models -I${LIBUSB_HOME}
+# You shouldn't need to change this
+common = -I${IDF_HOME}/models
 
+# LINUX
+ifeq (${TRICK_HOST_TYPE}, Linux)
+    # Change this if your location differs
+    LIBUSB_HOME = /usr/include/libusb-1.0
+
+    # You shouldn't need to change these
+    common += -I${LIBUSB_HOME}
+    TRICK_USER_LINK_LIBS += -lusb-1.0
+endif
+
+# MAC
+ifeq (${TRICK_HOST_TYPE}, Darwin)
+    # You shouldn't need to change this
+    TRICK_USER_LINK_LIBS += -framework IOKit -framework CoreFoundation
+endif
+
+# You shouldn't need to change these
 TRICK_CFLAGS += ${common}
 TRICK_CXXFLAGS += ${common}
-
-ifeq (${TRICK_HOST_TYPE},Linux)
-TRICK_USER_LINK_LIBS += -lusb-1.0
-endif
