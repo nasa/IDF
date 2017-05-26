5885ac6fece7aea8dc3e247d0d9e9cc1be0438cb
Derek Bankieris <derek.r.bankieris@nasa.gov>
2017-05-26 15:36:01 -0500

    Move Trickification support into its own makefile
    I gave up on trying to include IDF's dependencies in Trick's build
    process.
    core.mk now invokes Trick's usual behavior.
    trickified.mk uses Trickification but does not rebuild the core and
    Trickified libraries if IDF's source code changes.
    
    Refs #4

---
+++ 3rdParty/trick/makefiles/common.mk
@@ -0,0 +1,50 @@
+# This makefile sets up variables and rules common to all other makefiles.
+# The user need not include this from their S_overrides.mk.
+
+# Make IDF_HOME available to the sim at run time
+export IDF_HOME := $(abspath $(dir $(lastword $(MAKEFILE_LIST)))/../../..)
+TRICK_GTE_EXT += IDF_HOME
+
+# Header, SimObject, and Python module paths
+EXTERNALS         := externals/idf
+THIRD_PARTY       := $(IDF_HOME)/3rdParty/trick
+INCLUDE           := $(IDF_HOME)/include
+TRICK_CFLAGS      += -I$(INCLUDE)
+TRICK_CXXFLAGS    += -I$(INCLUDE)
+TRICK_SFLAGS      += -I$(THIRD_PARTY)/sim_objects
+TRICK_PYTHON_PATH += :$(EXTERNALS)/3rdParty/trick/python:$(THIRD_PARTY)/python
+TRICK_EXCLUDE     += :$(INCLUDE)/hidapi
+
+# Links to be built by build_externals
+LINKS := $(EXTERNALS)/apps/vhc/build $(EXTERNALS)/3rdParty/trick/python
+
+# Libraries
+ifeq ($(TRICK_HOST_TYPE), Linux)
+    TRICK_LDFLAGS += -ludev -lrt
+else ifeq ($(TRICK_HOST_TYPE), Darwin)
+    TRICK_LDFLAGS += -framework IOKit -framework CoreFoundation
+endif
+
+# Include libntcan, if available
+ifdef NTCAN_HOME
+    TRICK_CFLAGS   += -I$(NTCAN_HOME) -DIDF_CAN
+    TRICK_CXXFLAGS += -I$(NTCAN_HOME) -DIDF_CAN
+    TRICK_LDFLAGS  += -L$(NTCAN_HOME) -lntcan
+endif
+
+build_externals: $(LINKS)
+
+$(dir $(LINKS)):
+	@mkdir -p $@
+
+clean: clean_idf
+
+clean_idf:
+	@rm -rf externals/idf
+
+.SECONDEXPANSION:
+
+$(LINKS): $(EXTERNALS)% : $(IDF_HOME)% | $$(dir $$@)
+	@ln -s $< $@
+
+.PHONY: clean_idf
