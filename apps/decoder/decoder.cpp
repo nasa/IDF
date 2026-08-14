aa8001bd7c8bfb56e16430fc07de7a8ff93bdbee
Philip Kunz <philip.kunz@nasa.gov>
2026-08-14 14:19:34 -0500

    fix decoder app for devices that only provide values on change

--- apps/decoder/decoder.cpp
+++ apps/decoder/decoder.cpp
@@ -133,6 +133,7 @@ int main(int argc, char **args) {
 
             dataVect.clear();
             dataVect.assign(data, data + prevBytesRead);
+            if (dataVect.size() == 0) continue;
 
             // attempt to decode
 
