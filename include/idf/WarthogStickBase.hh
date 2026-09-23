42ed00d127c9a8f0974a48a0cfc3f8debc922cd2
Philip Kunz <philip.kunz@nasa.gov>
2026-09-23 12:09:59 -0500

    Warthog: rename inputs based on manual: https://ts.thrustmaster.com/download/accessories/pc/hotas/manual/HOTAS_Warthog/HOTAS_Warthog_Manual.pdf, but keep "button#" in name for generic usage with other compatible grips

--- include/idf/WarthogStickBase.hh
+++ include/idf/WarthogStickBase.hh
@@ -30,6 +30,8 @@ namespace idf
  * Thus, this base definition will only list the buttons. Each grip needs its
  * own layout
  *
+ * Manual with button numbers for included VIPER grip:
+ * https://ts.thrustmaster.com/download/accessories/pc/hotas/manual/HOTAS_Warthog/HOTAS_Warthog_Manual.pdf
  * @author Philip Kunz
  */
 class WarthogStickBase : public virtual InputLayout
@@ -47,52 +49,86 @@ public:
     /** left-right pivoting */
     SingleInput leftRightPivot;
 
-    /** hat north */
+    /** hat north (Trim Switch, grey hat, top left of face) */
     SingleInput hatNorth;
 
-    /** hat north east */
+    /** hat north east (Trim Switch, grey hat, top left of face) */
     SingleInput hatNorthEast;
 
-    /** hat east */
+    /** hat east (Trim Switch, grey hat, top left of face) */
     SingleInput hatEast;
 
-    /** hat south east */
+    /** hat south east (Trim Switch, grey hat, top left of face) */
     SingleInput hatSouthEast;
 
-    /** hat south */
+    /** hat south (Trim Switch, grey hat, top left of face) */
     SingleInput hatSouth;
 
-    /** hat south west */
+    /** hat south west (Trim Switch, grey hat, top left of face) */
     SingleInput hatSouthWest;
 
-    /** hat west */
+    /** hat west (Trim Switch, grey hat, top left of face) */
     SingleInput hatWest;
 
-    /** hat north west */
+    /** hat north west (Trim Switch, grey hat, top left of face) */
     SingleInput hatNorthWest;
 
     /** trigger */
     SingleInput trigger;
 
-    /** other buttons */
-    SingleInput button2;
-    SingleInput button3;
-    SingleInput button4;
-    SingleInput button5;
-    SingleInput button6;
-    SingleInput button7;
-    SingleInput button8;
-    SingleInput button9;
-    SingleInput button10;
-    SingleInput button11;
-    SingleInput button12;
-    SingleInput button13;
-    SingleInput button14;
-    SingleInput button15;
-    SingleInput button16;
-    SingleInput button17;
-    SingleInput button18;
-    SingleInput button19;
+    /** Weapon Release Button (Red, top left of face) */
+    SingleInput button2WeaponRelease;
+
+    /** Nose Wheel Steering (pinkie, opposite side, bottom of grip) */
+    SingleInput button3NoseSteering;
+
+    /** Paddle */
+    SingleInput button4Paddle;
+
+    /** Master Mode button (grey, right side, foreward of face) */
+    SingleInput button5MasterMode;
+
+    /** Trigger 2nd stage */
+    SingleInput button6Trigger2nd;
+
+    /** Button 7: Target Management System Up (Black ridged 4-way hat below Weapon Realease / button 2) */
+    SingleInput button7TmsUp;
+
+    /** Button 8: Target Management System Right (Black ridged 4-way hat below Weapon Realease / button 2) */
+    SingleInput button8TmsRight;
+
+    /** Button 9: Target Management System Down (Black ridged 4-way hat below Weapon Realease / button 2) */
+    SingleInput button9TmsDown;
+
+    /** Button 10: Target Management System Left (Black ridged 4-way hat below Weapon Realease / button 2) */
+    SingleInput button10TmsLeft;
+
+    /** Button 11: Data Management System Up (Black D-pad style 4-way hat, below the Grey 8 way hat) */
+    SingleInput button11DmsUp;
+
+    /** Button 12: Data Management System Right (Black D-pad style 4-way hat, below the Grey 8 way hat) */
+    SingleInput button12DmsRight;
+
+    /** Button 13: Data Management System Down (Black D-pad style 4-way hat, below the Grey 8 way hat) */
+    SingleInput button13DmsDown;
+
+    /** Button 14: Data Management System Up (Black D-pad style 4-way hat, below the Grey 8 way hat) */
+    SingleInput button14DmsLeft;
+
+    /** Button 15: Coutermeasure System Fwd (Thumbstick style 4-way hat) */
+    SingleInput button15CmsForward;
+
+    /** Button 16: Coutermeasure System Right (Thumbstick style 4-way hat) */
+    SingleInput button16CmsRight;
+
+    /** Button 17: Coutermeasure System Back (Thumbstick style 4-way hat) */
+    SingleInput button17CmsBack;
+
+    /** Button 18: Coutermeasure System Left (Thumbstick style 4-way hat) */
+    SingleInput button18CmsLeft;
+
+    /** Button 19: Coutermeasure System Push (Thumbstick style 4-way hat) */
+    SingleInput button19CmsPush;
 
 protected:
     virtual const std::vector<Configurable> & getConfigurables();
